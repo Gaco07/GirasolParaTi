7fc202883e2900ff3f19d2c1c39c66a60d01f579
Gaco07 <gcastro.odio@hotmail.com>
2026-09-21 22:07:56 -0600

    Change4

--- wwwroot/js/animacion.js
+++ wwwroot/js/animacion.js
@@ -1,18 +1,15 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
 
     const botonComenzar = document.getElementById("botonComenzar");
-
     const botonFinal = document.getElementById("botonFinal");
 
     const pantallaInicio = document.getElementById("pantallaInicio");
-
     const escena = document.querySelector(".escena");
-
     const mensajeFinal = document.getElementById("mensajeFinal");
-
     const pantallaFinal = document.getElementById("pantallaFinal");
 
 
+    // Botón "Toca aquí"
     botonComenzar.addEventListener("click", function () {
 
         pantallaInicio.classList.add("ocultar");
@@ -20,26 +17,25 @@
         escena.classList.add("iniciar");
 
 
-        // Después de 8 segundos desaparecen los girasoles
-
+        // Después de 3 segundos desaparecen los girasoles
         setTimeout(function () {
 
             escena.classList.add("ocultarGirasoles");
 
-        }, 8000);
+        }, 3000);
 
 
-        // Después de 9 segundos aparece el mensaje centrado
-
+        // Después de 3.5 segundos aparece el mensaje
         setTimeout(function () {
 
             mensajeFinal.classList.add("mostrar");
 
-        }, 9000);
+        }, 3500);
 
     });
 
 
+    // Botón de la flecha
     botonFinal.addEventListener("click", function () {
 
         pantallaFinal.classList.add("mostrar");
@@ -47,4 +43,3 @@
     });
 
 });
-```
