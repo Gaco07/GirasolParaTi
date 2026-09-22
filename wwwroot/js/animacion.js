9b76f3d2d59983d52cfecda489f5d40f05a098cc
Gaco07 <gcastro.odio@hotmail.com>
2026-09-21 21:01:37 -0600

    Add project files.

---
+++ wwwroot/js/animacion.js
@@ -0,0 +1,51 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const botonComenzar =
+        document.getElementById("botonComenzar");
+
+    const botonFinal =
+        document.getElementById("botonFinal");
+
+    const pantallaInicio =
+        document.getElementById("pantallaInicio");
+
+    const escena =
+        document.querySelector(".escena");
+
+    const mensajeFinal =
+        document.getElementById("mensajeFinal");
+
+    const pantallaFinal =
+        document.getElementById("pantallaFinal");
+
+
+    /* =========================================
+       COMENZAR
+    ========================================== */
+
+    botonComenzar.addEventListener("click", function () {
+
+        pantallaInicio.classList.add("ocultar");
+
+        escena.classList.add("iniciar");
+
+        setTimeout(function () {
+
+            mensajeFinal.classList.add("mostrar");
+
+        }, 5200);
+
+    });
+
+
+    /* =========================================
+       ÚLTIMA PÁGINA
+    ========================================== */
+
+    botonFinal.addEventListener("click", function () {
+
+        pantallaFinal.classList.add("mostrar");
+
+    });
+
+});
