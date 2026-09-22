aea4ffafa80ad424492e9ba6054d5fe3370b6e79
Gaco07 <gcastro.odio@hotmail.com>
2026-09-21 21:57:11 -0600

    Change3

--- wwwroot/js/animacion.js
+++ wwwroot/js/animacion.js
@@ -1,27 +1,17 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
 
-    const botonComenzar =
-        document.getElementById("botonComenzar");
+    const botonComenzar = document.getElementById("botonComenzar");
 
-    const botonFinal =
-        document.getElementById("botonFinal");
+    const botonFinal = document.getElementById("botonFinal");
 
-    const pantallaInicio =
-        document.getElementById("pantallaInicio");
+    const pantallaInicio = document.getElementById("pantallaInicio");
 
-    const escena =
-        document.querySelector(".escena");
+    const escena = document.querySelector(".escena");
 
-    const mensajeFinal =
-        document.getElementById("mensajeFinal");
+    const mensajeFinal = document.getElementById("mensajeFinal");
 
-    const pantallaFinal =
-        document.getElementById("pantallaFinal");
+    const pantallaFinal = document.getElementById("pantallaFinal");
 
-
-    /* =========================================
-       COMENZAR
-    ========================================== */
 
     botonComenzar.addEventListener("click", function () {
 
@@ -29,18 +19,26 @@
 
         escena.classList.add("iniciar");
 
+
+        // Después de 8 segundos desaparecen los girasoles
+
+        setTimeout(function () {
+
+            escena.classList.add("ocultarGirasoles");
+
+        }, 8000);
+
+
+        // Después de 9 segundos aparece el mensaje centrado
+
         setTimeout(function () {
 
             mensajeFinal.classList.add("mostrar");
 
-        }, 5200);
+        }, 9000);
 
     });
 
-
-    /* =========================================
-       ÚLTIMA PÁGINA
-    ========================================== */
 
     botonFinal.addEventListener("click", function () {
 
@@ -49,3 +47,4 @@
     });
 
 });
+```
